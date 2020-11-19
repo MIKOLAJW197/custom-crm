135ff4f187044e32605c3ac0d5851086ed2ac5cc
Mikolaj Wolicki <wolicki.mikolaj@gmail.com>
2020-11-19 20:54:38 +0100

    frontend: login/register mocks

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './auth.service';
 import { AuthComponent } from './auth.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
-  declarations: [AuthComponent],
+  declarations: [AuthComponent, LoginComponent, RegisterComponent],
   imports: [
     CommonModule
   ],
