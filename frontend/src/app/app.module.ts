f8f02c0e319b526d5882bf108c20b6f3039e6c96
Mikolaj Wolicki <wolicki.mikolaj@gmail.com>
2020-11-19 22:04:29 +0100

    frontend: add header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     AuthModule,
     ArticlesModule,
-    // SharedModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { HttpService } from './http.service';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [],
   providers: [HttpService],
-  exports: []
+  exports: [HeaderComponent]
 })
 export class SharedModule { }
