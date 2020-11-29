34c6ee62affcf7250e3bd28124313e8e9ac61693
Mikolaj Wolicki <wolicki.mikolaj@gmail.com>
2020-11-29 20:20:22 +0100

    backend: add get by user id

--- backend/src/articles/articles.controller.ts
+++ backend/src/articles/articles.controller.ts
@@ -1,6 +1,17 @@
 import { UpdateArticleDTO } from './dto/update-article.dto';
 import { PaginationQueryDto } from './../common/dto/pagination-query.dto';
-import { Controller, Get, Query, Param, Post, Body, Patch, Delete, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Post,
+  Body,
+  Patch,
+  Delete,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDTO } from './dto/create-article.dto';
 import JwtAuthenticationGuard from 'src/authentication/guards/jwt-authentication.guard';
@@ -11,33 +22,42 @@ import { ApiTags } from '@nestjs/swagger';
 @ApiTags('articles')
 @Controller('articles')
 export class ArticlesController {
-    constructor(private readonly articleSerivce: ArticlesService) { }
+  constructor(private readonly articleSerivce: ArticlesService) {}
 
-    @Get()
-    findAll(@Query() paginationQuery: PaginationQueryDto) {
-        return this.articleSerivce.findAll(paginationQuery);
-    }
+  @Get()
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.articleSerivce.findAll(paginationQuery);
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.articleSerivce.findOne(id);
-    }
+  @Get('user')
+  @UseGuards(JwtAuthenticationGuard)
+  findAllByUser(@Req() { user }: RequestWithUser) {
+    return this.articleSerivce.findByUser(user);
+  }
 
-    @Post()
-    @UseGuards(JwtAuthenticationGuard)
-    create(@Body() createArticleDto: CreateArticleDTO, @Req() { user }: RequestWithUser) {
-        return this.articleSerivce.create(createArticleDto, user);
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.articleSerivce.findOne(id);
+  }
 
-    @Patch(':id')
-    @UseGuards(JwtAuthenticationGuard, ArticleAuthorGuard)
-    update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDTO) {
-        return this.articleSerivce.update(id, updateArticleDto);
-    }
+  @Post()
+  @UseGuards(JwtAuthenticationGuard)
+  create(
+    @Body() createArticleDto: CreateArticleDTO,
+    @Req() { user }: RequestWithUser,
+  ) {
+    return this.articleSerivce.create(createArticleDto, user);
+  }
 
-    @Delete(':id')
-    @UseGuards(JwtAuthenticationGuard, ArticleAuthorGuard)
-    remove(@Param('id') id: string) {
-        return this.articleSerivce.remove(id);
-    }
+  @Patch(':id')
+  @UseGuards(JwtAuthenticationGuard, ArticleAuthorGuard)
+  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDTO) {
+    return this.articleSerivce.update(id, updateArticleDto);
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtAuthenticationGuard, ArticleAuthorGuard)
+  remove(@Param('id') id: string) {
+    return this.articleSerivce.remove(id);
+  }
 }
