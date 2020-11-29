8d2e71816a6999efa72e79777064957359c04ff5
Mikolaj Wolicki <wolicki.mikolaj@gmail.com>
2020-11-29 14:54:31 +0100

    frontend: admin panel

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { AddArticleComponent } from './add-article/add-article.component';
+import { MyArticlesComponent } from './my-articles/my-articles.component';
+import { AdminArticleComponent } from './admin-article/admin-article.component';
 
 
 
 @NgModule({
-  declarations: [AdminPanelComponent, AddArticleComponent],
+  declarations: [AdminPanelComponent, AddArticleComponent, MyArticlesComponent, AdminArticleComponent],
   imports: [
     CommonModule
   ]
